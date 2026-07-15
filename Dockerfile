d746482ec9874dc05a23a39f3f8bf20edea4f8b7
anmolsharan <anmolsharan@gmail.com>
2026-07-15 15:21:42 +0530

    Update for Google Cloud Run

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 7860
+EXPOSE 8080
 
-CMD ["python", "app.py"]
+CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -443,6 +443,6 @@ def download(filename):
 # -------------------- Run Server --------------------
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
